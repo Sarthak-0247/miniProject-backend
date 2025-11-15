bdc85b5a8088867031c0caf907f53f49d3f067da
Sarthak Singh <sarthaksinghcpj@gmail.com>
2025-11-15 14:20:11 +0530

    fresh start

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.get("/" , (req , res) =>{
+    res.send("hello from backend");
+})
+app.get("/testing" , (req,res) => {
+    res.json({
+        success : true,
+        data : {
+            name : "sachin",
+            no : "885975"
+        }
+    })
+})
+export default app;
